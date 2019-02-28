6ce3b305dc812f1464f007eb1053a9604ae466ac
Alop <47197418+Phayze@users.noreply.github.com>
2019-02-27 20:51:26 -0500

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,13 @@
+/*function Child(name, gender, age){
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+
+  this.describe = function(){
+    return  this.name+ " is a "+ this.age+ " year old man."
+  }
+}
+var david = new Child("David","Male", "48")
+console.log(david.describe())
+*/
+
